c885291050759520c38d002ba529a4e85512ed24
martin petik <279832@student.calderdale.ac.uk>
2025-04-08 18:09:20 +0100

    new aur pkg

--- bta.sh
+++ bta.sh
@@ -1,7 +1,12 @@
 #Bash-Theft-Auto music and sfx © 2024 by stuffbymax - Martin Petik is licensed under CC BY 4.0
 #https://creativecommons.org/licenses/by/4.0/
-#!/bin/bash
 #ver 2.0.2
+
+#!/bin/bash
+
+# Set correct working directory
+BASEDIR="$(dirname "$(realpath "$0")")"
+
 
 # --- 0. Global Variables ---
 player_name=""
